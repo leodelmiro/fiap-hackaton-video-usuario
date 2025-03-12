f10d148f06519fdade78d70513d42089fda4c247
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-03-12 14:24:05 -0300

    feat: initial commit

---
+++ src/main/kotlin/com/leodelmiro/usuario/UsuarioApplication.kt
@@ -0,0 +1,11 @@
+package com.leodelmiro.usuario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsuarioApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsuarioApplication>(*args)
+}
